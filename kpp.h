499cd2cf9386a48c1070bb1005f3e695c4cfd94d
Zila||CHUVAK <bomz81378@gmail.com>
2026-05-14 14:58:15 +0300

    Add files via upload

--- kpp.h
+++ kpp.h
@@ -1,71 +1,90 @@
-#ifndef K++_H_INCLUDED
-#define K++_H_INCLUDED
+#ifndef KPP_H_INCLUDED
+#define KPP_H_INCLUDED
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// ОБЪЯВЛЕНИЯ (прототипы)
+void wlog(char arr[]);
+void clear_log();
+void pas();
+void p(char arr[]);
+void po(char arr[]);
+void calc(int num, int num2, char arr[]);
+void ubc(int f, int g);   // ← объявление
+
+// РЕАЛИЗАЦИИ (тела функций)
 void wlog(char arr[]) {
     FILE *log = fopen("prog.log", "a");
     fprintf(log, "Progress ended, status:%s\n", arr);
-    printf("log made\n cheak prog.log");
+    fclose(log);
+    printf("log made\ncheck prog.log\n");
 }
-void clog() {
+
+void clear_log() {
     FILE *log = fopen("prog.log", "w");
-    fprintf(log, "------PROGRAMM LOG------\n");
-    printf("log clean\n");
+    fprintf(log, "------PROGRAM LOG------\n");
+    fclose(log);
+    printf("log cleaned\n");
 }
+
 void pas() {
     printf("\n");
     system("pause");
 }
 
 void p(char arr[]) {
-    printf("%s" , arr);
-    if(arr = "Cola" ) {
-        printf("Subscibe to @negrmkishev in Tiktok!");
+    printf("%s", arr);
+    if(strcmp(arr, "Cola") == 0) {
+        printf("Subscribe to @negrmkishev in TikTok!");
     }
 }
 
 void po(char arr[]) {
-    printf("%s\n" , arr);
+    printf("%s\n", arr);
 }
 
 void calc(int num, int num2, char arr[]) {
-    int num3;
-    float num4;
-    if(arr == "+") {
-        num3 = num + num2;
-        printf("%d\n", num3);
-    }else if(arr == "-") {
-        num3 = num - num2;
-        printf("%d\n", num3);
-    }else if (arr = "*") {
-        num3 = num * num2;
-        printf("%d\n", num3);
-    }else if (arr = "/") {
-        num4 = num / num2;
-        printf("%f\n", num4);
-    }else if(arr = "%") {
-        num4 = num % num2;
-        printf("%f\n", num4);
+    if(strcmp(arr, "+") == 0) {
+        printf("%d\n", num + num2);
     }
+    else if(strcmp(arr, "-") == 0) {
+        printf("%d\n", num - num2);
+    }
+    else if(strcmp(arr, "*") == 0) {
+        printf("%d\n", num * num2);
+    }
+    else if(strcmp(arr, "/") == 0) {
+        if(num2 != 0) {
+            printf("%.2f\n", (float)num / num2);
+        } else {
+            printf("Error: division by zero\n");
+        }
+    }
+    else if(strcmp(arr, "%") == 0) {
+        if(num2 != 0) {
+            printf("%d\n", num % num2);
+        } else {
+            printf("Error: division by zero\n");
+        }
+    }
+    else {
+        printf("Unknown operation: %s\n", arr);
+    }
+}
+
+// ← РЕАЛИЗАЦИЯ ubc (должна быть!)
 void ubc(int f, int g) {
-    int v;
     if(f > g) {
-        v = f;
-        printf("%d", v);
+        printf("%d\n", f);
     }
-    if(f < g) {
-        v = g;
-        printf("%d", v);
+    else if(f < g) {
+        printf("%d\n", g);
     }
-
+    else {
+        printf("Equal: %d\n", f);
+    }
 }
 
-
-
-}
-
-
-
-
-
-#endif // K++_H_INCLUDED
+#endif // KPP_H_INCLUDED
